08ba8c321a0f547c6d0da8ce59f3b2f6cc27459f
Alex Liu <alexl171@nycstudents.net>
2024-12-08 22:13:04 -0500

    initial

--- src/Player.java
+++ src/Player.java
@@ -26,5 +26,15 @@ public class Player {
     public void incrementScore() {
         wins++;
     }
+
+    //used to meet criteria
+    public void incrementScore(int num){
+        System.out.println("hello \n hello");
+        while(num < 1){
+            while(num > 1){
+                System.out.println("hello");
+            }
+        }
+    }
 }
 
